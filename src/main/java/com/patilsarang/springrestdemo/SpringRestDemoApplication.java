3bea02da1c012f50f523196a3da8a80b23c858f3
Sarang Shalikrao Patil <sarang.patil@Yash.com>
2019-08-30 13:28:06 +0530

    first setup

---
+++ src/main/java/com/patilsarang/springrestdemo/SpringRestDemoApplication.java
@@ -0,0 +1,15 @@
+package com.patilsarang.springrestdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class SpringRestDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestDemoApplication.class, args);
+	}
+
+}
